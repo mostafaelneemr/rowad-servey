b98f901738e8a7a0c3ad162697f9a9c6422a071f
mostafaelneemr <mostafa.elnemr50@gmail.com>
2025-05-06 01:35:57 +0300

    update service

--- resources/views/web/call.blade.php
+++ resources/views/web/call.blade.php
@@ -1,21 +1,21 @@
 <!-- Start Call To Action -->
-<div class="brook-call-to-action ptb--70" style="background-color: #222933">
+<div class="brook-call-to-action pb--70" style="background-color: #fff; margin-top: -50px">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-12 text-center">
-                <div class="d-inline-flex gap-2 flex-wrap justify-content-center">
-                    <a class="brook-btn bk-btn-white text-theme btn-sd-size btn-rounded"
-                       style="background-color: rgba(12,128,41,1); color: whitesmoke"
-                       href="https://wa.me/{{ setting('whatsapp_phone')->value }}" target="_blank">
-                        {{ __('WhatsApp') }}
-                        <i class="fab fa-whatsapp" style="padding-left: 5px;"></i>
-                    </a>
-
+                <div class="d-inline-flex gap-4 flex-wrap justify-content-center">
                     <a class="brook-btn bk-btn-white text-theme btn-sd-size btn-rounded"
                        style="background-color: rgba(255,171,0,1); color: whitesmoke"
                        href="mailto:{{ setting('email')->value }}" target="_blank">
                         {{ __('Email') }}
                         <i class="fa fa-envelope" style="padding-left: 5px;"></i>
+                    </a>
+
+                    <a class="brook-btn bk-btn-white text-theme btn-sd-size btn-rounded"
+                       style="background-color: rgba(12,128,41,1); color: whitesmoke"
+                       href="https://wa.me/{{ setting('whatsapp_phone')->value }}" target="_blank">
+                        {{ __('WhatsApp') }}
+                        <i class="fab fa-whatsapp" style="padding-left: 5px;"></i>
                     </a>
                 </div>
 
